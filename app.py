cf5e2ff2841d56cce2c404e775f35493feac6e51
Patrik Turzák <pato.turzak@gmail.com>
2023-05-11 17:52:33 +0200

    Remove unnecessary dbc import.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 from dash import Dash, dcc, html, Input, Output
-import dash_bootstrap_components as dbc
 
 app = Dash(__name__)
 server = app.server 
